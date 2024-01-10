887191e7f4d8028fca7c2a754ae7980b31322fea
olivier35290 <olivier.charlet@neuf.fr>
2024-01-10 09:35:43 +0000

    exercice 1 types et variables

---
+++ types-et-variables/Exercice 1/exercice-2.php
@@ -0,0 +1,16 @@
+<?php
+
+//Déclarez un tableau de nombres appelé `$nb_tab` qui contient des `int` et des `float` et affichez-le en utilisant `print_r`.
+$nb_tab=[1, 1.5, 2, 2.5];
+print_r ($nb_tab);
+echo "<br>";
+
+//Déclarez un tableau de nombres appelé `$str_tab` qui contient des `string` et affichez-le en utilisant `print_r`.
+$str_tab=["Olivier", "Louis", "Clement", "Coline"];
+print_r ($nb_tab);
+echo "<br>";
+
+//La documentation de `print_r` : https://www.php.net/manual/fr/function.print-r.php
+
+
+?>
