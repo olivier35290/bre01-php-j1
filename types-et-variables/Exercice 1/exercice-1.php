f0e9bdf238d1bd5be3147fba80dd4276198be411
olivier35290 <olivier.charlet@neuf.fr>
2024-01-10 09:26:26 +0000

    exercice 1 types et variables

---
+++ types-et-variables/Exercice 1/exercice-1.php
@@ -0,0 +1,18 @@
+<?php
+
+//Déclarez une variable `$nb` qui prend la valeur `42` et affichez-la avec un `echo`.
+$nb=42;
+echo $nb;
+echo "<br>";
+
+//Déclarez une variable `$str` qui prend la valeur `'42'` et affichez-la avec un `echo`.
+$str='42';
+echo $str;
+echo "<br>";
+
+//Déclarez une variable `$nbstr` qui est une string, injectez-y la variable `$nb` et affichez-la avec un `echo`.
+$nbstr=(string)$nb;
+echo $nbstr;
+echo "<br>";
+
+?>
