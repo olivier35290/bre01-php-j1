db1262e1f648b337e2443b6d11258d17ca9e1fdc
olivier35290 <olivier.charlet@neuf.fr>
2024-01-10 13:18:41 +0000

    exercice 1 types et variables

--- fonctions/exercice-2.php
+++ fonctions/exercice-2.php
@@ -1,10 +1,26 @@
 <?php
 
-/*
+/*Vous allez devoir créér une fonction appellée `average`, elle prend un tableau en paramètre et renvoie un `float`.
 
+Cette fonction calcule la moyenne des nombres contenus dans le tableau contenu en paramètres et la renvoie.
 
+### Tester votre fonction
+
+```php
+echo average([12, 15, 18, 9])."<br>";
+echo average([12, 15, 18, 11, 14])."<br>";
 */
+function average($number)
+{
+    $sum = array_sum($number);
+    $size = count($number);
+    
+    return $sum / $size;
+   
+}
 
 
+echo average([12, 15, 18, 9])."<br>";
+echo average([12, 15, 18, 11, 14])."<br>";
 
 ?>
