e090c5f70ad97d6e82d51262e403617da262bcbe
olivier35290 <olivier.charlet@neuf.fr>
2024-01-10 15:28:35 +0000

    exercice 1 types et variables

--- fonctions/exercice-4.php
+++ fonctions/exercice-4.php
@@ -27,4 +27,22 @@ echo "<br>";
 var_dump(getFirst([]));
 echo "<br>";
 
+/* OU : 
+<?php
+
+function getFirst(array $tab) : ? int
+{
+    if(count($tab) > 0)
+        return $tab[0];
+
+    return null;
+}
+
+var_dump(getFirst([13, 24, 45]));
+echo "<br>";
+var_dump(getFirst([]));
+echo "<br>";
+*/
+
+
 ?>
