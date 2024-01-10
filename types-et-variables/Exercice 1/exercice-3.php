ece8c964768285392edd67deeaf3558c5e68dfb3
olivier35290 <olivier.charlet@neuf.fr>
2024-01-10 09:47:15 +0000

    exercice 1 types et variables

---
+++ types-et-variables/Exercice 1/exercice-3.php
@@ -0,0 +1,14 @@
+<?php
+
+//Créez un tableau associatif `$animal` qui aura les clés `"species"`, `"name"` et `"age"`.
+$animal = [
+    "species"=>"Chien",
+    "name"=>"Umy",
+    "age"=>1
+    ];
+
+
+//Mettez-y les valeurs que vous voulez et affichez-le en utilisant `print_r`.
+print_r ($animal);
+
+?>
